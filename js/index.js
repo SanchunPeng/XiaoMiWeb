d81cbacf284bfdcdb282c4821169644c1436d19a
SanchunPeng <1024884152@qq.com>
2017-06-10 23:49:38 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 $(function(){
+	//h_t_r_shopCart动画
 	$(".h_t_r_shopCart").hover(function(){
 		$(".h_m_r_s_hide").stop(true).slideDown();	
 	},function(){
 		$(".h_m_r_s_hide").stop(true).slideUp();
 	})
-	
+	//w_n_serach 动画
+	$(".w_n_s_input input").focus(function(){
+		$(".w_n_serach").addClass("focus");
+		$(".w_n_s_hide").fadeIn();	
+	}).blur(function(){
+		$(".w_n_serach").removeClass("focus");
+		$(".w_n_s_hide").fadeOut();
+		
+	})
 })
